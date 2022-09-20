6594b67ee7a8a3ad68614d3939cec87c69a8f15c
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-20 11:30:20 -0700

    complete all functions and pass all tests

--- reduce.js
+++ reduce.js
@@ -2,9 +2,20 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
-
+export const getSneaks = (brands) => {
+    return brands.reduce((array, brands) => {
+        array.push(...brands.shoes);
+        return array;
+    }, []);
+};
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+    // let totalAge = 0;
+    // let age = 0;
+    return dogs.reduce((avg, dog) => avg += dog.age, 0) / dogs.length;
+};
+
+
+
